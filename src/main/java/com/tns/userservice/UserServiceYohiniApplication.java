579c278cc283a7910434d42738075e8a77a3cfce
Yohini K <130301112+Yohinikandavel@users.noreply.github.com>
2024-04-07 19:21:27 +0530

    User Service for Shopping mall project

---
+++ src/main/java/com/tns/userservice/UserServiceYohiniApplication.java
@@ -0,0 +1,14 @@
+package com.tns.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceYohiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceYohiniApplication.class, args);
+	}
+
+}
+	
